e94fb7902cc12430d3e054a63372e571d7afd77e
MaxK0 <maxkoryakov1@gmail.com>
2024-02-20 18:26:27 +0500

    Переделан AdminLTE для своего сайта

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Main\IndexController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,8 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/admin', [AdminController::class, 'index'])->name('admin');
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [IndexController::class, 'index'])->name('main');
