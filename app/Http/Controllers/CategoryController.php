09992a815841e8b6ac2deaae6ea3f8c350951d0b
MaxK0 <maxkoryakov1@gmail.com>
2024-02-21 19:53:59 +0500

    Созданы миграция, модель и роуты для категорий

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+    }
+
+    public function create()
+    {
+    }
+
+    public function store()
+    {
+    }
+
+    public function show(int $category)
+    {
+    }
+
+    public function edit(int $category)
+    {
+    }
+
+    public function update(int $category)
+    {
+    }
+
+    public function destroy(int $category)
+    {
+    }
+}
