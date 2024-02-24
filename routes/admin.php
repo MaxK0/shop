099d0b2b1d1946a2655f243ec37b603a25544ca6
MaxK0 <maxkoryakov1@gmail.com>
2024-02-24 22:45:59 +0500

    Добавлен префикс для названия роутов

--- routes/admin.php
+++ routes/admin.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\CategoryController;
 
-Route::prefix('admin')->group(function() {
-    Route::get('/', [AdminController::class, 'index'])->name('admin.index');
+Route::prefix('admin')->name('admin.')->group(function() {
+    Route::get('/', [AdminController::class, 'index'])->name('index');
     Route::resource('categories', CategoryController::class);
 });
