83161c33c785a573a1d8cba748a4e061acd7a6dc
MaxK0 <maxkoryakov1@gmail.com>
2024-02-24 22:44:47 +0500

    Обработан контроллер категорий

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -3,36 +3,56 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Category\StoreRequest;
+use App\Http\Requests\Admin\Category\UpdateRequest;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
     public function index()
     {
-        return view('admin.category.index');
+        $categories = Category::all();
+        return view('admin.category.index', compact('categories'));
     }
 
     public function create()
     {
+        return view('admin.category.create');
     }
 
-    public function store()
-    {
+    public function store(StoreRequest $request)
+    {        
+        $data = $request->validated();
+        Category::create($data);
+
+        return redirect()->route('admin.category.index');
     }
 
-    public function show(int $category)
+    public function show(Category $category)
     {
+        return view('admin.category.show', compact('category'));
     }
 
-    public function edit(int $category)
+    public function edit(Category $category)
     {
+        return view('admin.category.edit', compact($category));
     }
 
-    public function update(int $category)
+    public function update(UpdateRequest $request, Category $category)
     {
+        $request->id = $category->id;
+        $data = $request->validated();
+
+        $category->update($data);
+
+        return redirect()->route('admin.category.index');
     }
 
-    public function destroy(int $category)
+    public function destroy(Category $category)
     {
+        $category->delete();
+        
+        return redirect()->route('admin.category.index');
     }
 }
