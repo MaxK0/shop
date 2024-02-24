b20ab72bd3800deae255ad99f5ce0a6682f747ab
MaxK0 <maxkoryakov1@gmail.com>
2024-02-24 22:50:02 +0500

    Созданы страницы редактирования, показа категорий

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Dashboard</title>
+  <title>@yield('title', 'Админка')</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -155,7 +155,7 @@
             </a>
           </li>
           <li class="nav-item">
-            <a href="pages/gallery.html" class="nav-link">
+            <a href="{{ route('admin.categories.index') }}" class="nav-link">
                 <i class="nav-icon fas fa-list-ul"></i>
               <p>
                 Категории
