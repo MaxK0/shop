648e4a310a5e33b847a40ec303b8addd797dbad3
MaxK0 <maxkoryakov1@gmail.com>
2024-02-26 22:22:27 +0500

    Реализована серверная часть для crud'а пользователей

---
+++ app/Http/Requests/Admin/User/StoreRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\Admin\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'surname' => ['nullable', 'string', 'max:255'],
+            'lastname' => ['nullable', 'string', 'max:255'],
+            'password' => ['required', 'confirmed', 'min:6', 'max:255'],
+            'email' => ['required', 'email', 'max:255', Rule::unique('users', 'email')],
+            'age' => ['nullable', 'integer'],
+            'address' => ['nullable', 'string', 'max:255'],
+            'gender' => ['nullable', 'integer'],
+        ];
+    }
+}
--- routes/admin.php
+++ routes/admin.php
@@ -5,10 +5,12 @@ use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\ColorController;
 use App\Http\Controllers\Admin\TagController;
+use App\Http\Controllers\Admin\UserController;
 
 Route::prefix('admin')->name('admin.')->group(function() {
     Route::get('/', [AdminController::class, 'index'])->name('index');
     Route::resource('categories', CategoryController::class);
     Route::resource('tags', TagController::class);
     Route::resource('colors', ColorController::class);
+    Route::resource('users', UserController::class);
 });
