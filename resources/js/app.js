e261ec2b49ce217213c8e532f56cf59e2e596827
MaxK0 <maxkoryakov1@gmail.com>
2024-04-15 21:42:24 +0500

    Создан vuex

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,8 +2,10 @@ import './bootstrap';
 import { createApp } from "vue";
 import App from './src/App.vue';
 import Router from "./src/router/router.js";
+import Store from "./src/store/store.js";
 import './src/assets/css/style.css';
 
 createApp(App)
     .use(Router)
+    .use(Store)
     .mount('#app');
