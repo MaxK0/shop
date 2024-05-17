b3d441d498935d23c3248bd69734499d048429c1
MaxK0 <maxkoryakov1@gmail.com>
2024-05-17 23:48:58 +0500

    Сделан компонент выбора

--- resources/views/components/form-group.blade.php
+++ resources/views/components/form-group.blade.php
@@ -4,14 +4,29 @@
     'required' => false,
     'label' => '',
     'placeholder' => '',
-    'value' => '',
+    'values' => [],
+    'value' => old($name),
+    'valueOption' => 'id',
+    'multiple' => false,
+    'title' => 'title',
+    'selectEdit' => null
 ])
 
 <div class="form-group">
     <label class="{{ $required ? 'required' : '' }}" for="{{ $name }}">{{ __($label) }}</label>
-    <input class="form-control @error('{{ $name }}') is-invalid @enderror" name="{{ $name }}" type="{{ $type }}" placeholder="{{ $placeholder }}"
-        value="{{ $value }}">
-    @error('{{ $name }}')
-        <span class="invalid error-feedback">{{ $message }}</span>
-    @enderror
+    @if ($type === 'select')
+        <select class="form-control select2" name="{{ $name }}" data-placeholder="{{ $placeholder }}"
+            {{ $multiple ? "multiple = 'multiple'" : '' }} style="width: 100%">
+            @if (!empty($values))
+                @foreach ($values as $val)
+                    <option value="{{ $val->$valueOption }}" {{ (old($name) ?? $selectEdit) === $val->$valueOption ? 'selected' : '' }}>
+                        {{ $val->$title }}</option>
+                @endforeach
+            @endif
+        </select>
+    @else
+        <input class="form-control @error('{{ $name }}') is-invalid @enderror" name="{{ $name }}"
+            type="{{ $type }}" placeholder="{{ $placeholder }}" value="{{ $value }}">
+    @endif
+    <x-error :name=$name></x-error>
 </div>
